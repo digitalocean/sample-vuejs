7570c9868cc0a3cd6c9fe909189b44f1d441bcb3
MegaThawt <johnpurnell@users.noreply.github.com>
2020-10-16 09:45:20 -0600

    Update App.vue
    changed background color

--- src/App.vue
+++ src/App.vue
@@ -31,7 +31,7 @@ export default {
 
 <style>
 #app {
-  background-color: #268351;
+  background-color: #ADD8E6;
   background-image: url("data:image/svg+xml,%3Csvg width='60' height='60' viewBox='0 0 60 60' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='none' fill-rule='evenodd'%3E%3Cg fill='%232c945c' fill-opacity='0.4'%3E%3Cpath d='M36 34v-4h-2v4h-4v2h4v4h2v-4h4v-2h-4zm0-30V0h-2v4h-4v2h4v4h2V6h4V4h-4zM6 34v-4H4v4H0v2h4v4h2v-4h4v-2H6zM6 4V0H4v4H0v2h4v4h2V6h4V4H6z'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E");
 }
 </style>
